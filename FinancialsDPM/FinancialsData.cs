463a2cbaf028d63e7d33566d4da2a4c5ab267788
Interseptor06 <martin@chtilianov.com>
2022-01-30 12:44:00 +0200

    Testing Commit and Branch

--- FinancialsDPM/FinancialsData.cs
+++ FinancialsDPM/FinancialsData.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace LexonicDataPipelineAndDBComms
 {
     public class FinancialsData
     {
-        
+        public static void gitBranchTest()
+        {
+            Console.WriteLine("Ciganin");
+        }    
     }
 }
